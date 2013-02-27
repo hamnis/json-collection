60821946097bb484353c170c8a6094d7e29c5c09
Erlend Hamnaberg <erlend@hamnaberg.net>
2013-02-27 15:38:19 +0100

    Expand Query string correctly

--- src/main/java/net/hamnaberg/json/URITarget.java
+++ src/main/java/net/hamnaberg/json/URITarget.java
@@ -1,6 +1,13 @@
 package net.hamnaberg.json;
 
+import net.hamnaberg.json.util.Optional;
+import net.hamnaberg.json.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.List;
 
 public class URITarget implements Target {
@@ -24,7 +31,66 @@ public class URITarget implements Target {
     }
 
     public URI expand(List<Property> properties) {
-        throw new UnsupportedOperationException("Not implemented");
+        final String query = href.getQuery();
+        StringBuilder fromProperties = buildQuery(properties);
+        if (StringUtils.isNotBlank(query)) {
+            String actual = query;
+            if (fromProperties.length() > 0) {
+                actual = query + "&";
+            }
+            fromProperties.insert(0, actual);
+        }
+        try {
+            return new URI(
+                    href.getScheme(),
+                    href.getUserInfo(),
+                    href.getHost(),
+                    href.getPort(),
+                    href.getPath(),
+                    fromProperties.length() == 0 ? null : fromProperties.toString(),
+                    href.getFragment()
+            );
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private StringBuilder buildQuery(List<Property> properties) {
+        StringBuilder sb = new StringBuilder();
+        for (Property property : properties) {
+            if (property.isObject()) {
+                throw new IllegalArgumentException("Expanding Cj Property object is undefined in the spec.");
+            }
+            else {
+                if (property.isArray()) {
+                    List<Value> prop = property.getArray();
+                    for (Value value : prop) {
+                        if (sb.length() > 0) {
+                            sb.append("&");
+                        }
+                        sb.append(property.getName()).append("=").append(encode(value.asString()));
+                    }
+                }
+                else {
+                    Optional<Value> value = property.getValue();
+                    if (value.isSome()) {
+                        if (sb.length() > 0) {
+                            sb.append("&");
+                        }
+                        sb.append(property.getName()).append("=").append(encode(value.get().asString()));
+                    }
+                }
+            }
+        }
+        return sb;
+    }
+
+    private String encode(String param) {
+        try {
+            return URLEncoder.encode(param, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new UnsupportedCharsetException("UTF-8");
+        }
     }
 
     @Override
--- src/main/java/net/hamnaberg/json/util/StringUtils.java
+++ src/main/java/net/hamnaberg/json/util/StringUtils.java
@@ -8,4 +8,12 @@ public final class StringUtils {
     public static String capitalize(String name) {
         return Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
+
+    public static boolean isBlank(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+
+    public static boolean isNotBlank(String input) {
+        return !isBlank(input);
+    }
 }
