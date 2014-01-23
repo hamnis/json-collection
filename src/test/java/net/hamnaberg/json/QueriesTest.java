f462a752e4fbdf6c5d0d0c047d7bab02515a08f5
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-01-23 09:40:10 +0100

    Moar tests.

---
+++ src/test/java/net/hamnaberg/json/QueriesTest.java
@@ -0,0 +1,23 @@
+package net.hamnaberg.json;
+
+import net.hamnaberg.funclite.Optional;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class QueriesTest {
+
+    @Test
+    public void findCreatedQuery() throws Exception {
+        Query query = Query.create(new URITemplateTarget("http://example.com{?q}"), "filter", Optional.<String>none(), Collections.singletonList(Property.template("q")));
+        Collection collection = Collection.builder().addQuery(query).build();
+        Query filter = collection.queryByRel("filter").get();
+        assertThat(URI.create("http://example.com"), equalTo(filter.expand()));
+        assertThat(URI.create("http://example.com?q=faff"), equalTo(filter.expand(Collections.singletonList(Property.value("q", "faff")))));
+
+    }
+}
