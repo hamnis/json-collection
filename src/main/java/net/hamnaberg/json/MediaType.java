2b5d244bb404049c8c29bd86056a9dc95b8fd794
cfinckler <christian.finckler@otto.de>
2014-02-25 11:00:18 +0100

    Added class with mediatype constants

---
+++ src/main/java/net/hamnaberg/json/MediaType.java
@@ -0,0 +1,11 @@
+package net.hamnaberg.json;
+
+public final class MediaType {
+	public static final String COLLECTION_JSON_MIME_TYPE = "application";
+	public static final String COLLECTION_JSON_MIME_SUBTYPE = "vnd.collection+json";
+	public static final String COLLECTION_JSON = COLLECTION_JSON_MIME_TYPE+"/"+COLLECTION_JSON_MIME_SUBTYPE;
+	
+	private MediaType(){}
+	
+
+}
